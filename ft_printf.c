7e2a6988c463dfec61cee1dc8d966e57cccc9c21
Mike <mchen300@gmail.com>
2017-02-20 01:19:21 +0000

    prints out string rather than a character at a time

--- ft_printf.c
+++ ft_printf.c
@@ -19,22 +19,20 @@ int	ft_vfdprintf(int fd, const char *format, va_list a_list)
 	size_t		count;
 
 	count = 0;
-	if (format)
+	if (e = format)
 	{
 		p = malloc(sizeof(*p));
-		e = format;
-		while (*format)
+		while (*e)
 		{
-			if (*format == '%')
+			if (*e == '%')
 			{
-				e = format + 1;
+				count += ft_putnstr_fd(fd, format, e++ - format);
 				eval_fields(p, &e, a_list);
 				count += print_eval(fd, p, a_list, count);
-				format = e;
+				format = e + 1;
 			}
-			else
-				count += ft_putchar_fd(*(format), fd);
-			format += *format ? 1 : 0;
+			if (!*(e += *e ? 1 : 0))
+				count += ft_putnstr_fd(fd, format, e - format);
 		}
 		free(p);
 	}
@@ -69,7 +67,62 @@ int	ft_printf(const char *format, ...)
 int main()
 {
     setlocale(LC_CTYPE, "");
+ /*   ft_printf("ft_printf: %d % 0.10d %li %%\n", 13, 18, 3049600000);
+       printf("ac_printf: %d % 0.10d %li %%\n", 13, 18, 3049600000);
+    ft_printf("ft_printf: %d %-#*lx %i %%\n", 13, -18, 3049600000, -4078);
+       printf("ac_printf: %d %-#*lx %i %%\n", 13, -18, 3049600000, -4078);
+    ft_printf("ft_printf: %d % 0*.10ld %i %%\n", 13, 18, 3049600000, -4078);
+       printf("ac_printf: %d % 0*.10ld %i %%\n", 13, 18, 3049600000, -4078);
+    ft_printf("ft_printf: %d %-*lx %i %%\n", 13, 18, 3049600000, -4078);
+       printf("ac_printf: %d %-*lx %i %%\n", 13, 18, 3049600000, -4078);
+    ft_printf("ft_printf: %d %-*lo %i %%\n", 13, 18, 3049600000, -4078);
+       printf("ac_printf: %d %-*lo %i %%\n", 13, 18, 3049600000, -4078);
+    ft_printf("ft_printf: %d -%6c- %i %%\n", 13, 0, -4078);
+       printf("ac_printf: %d -%6c- %i %%\n", 13, 0, -4078);
+    ft_printf("ft_printf: %d %6c %i %%\n", 13, L'¥', -4078);
+       printf("ac_printf: %d %6c %i %%\n", 13, L'¥', -4078);
+    ft_printf("ft_printf: %d %-10c %i %%\n", 13, 50, -4078);
+       printf("ac_printf: %d %-10c %i %%\n", 13, 50, -4078);
+    ft_printf("ft_printf: %d %-*c %i %%\n", 13, 18, 333, -4078);
+       printf("ac_printf: %d %-*c %i %%\n", 13, 18, 333, -4078);
+    ft_printf("ft_printf: %d %-*.5s %i %%\n", 13, 18, "testing 123", -4078);
+       printf("ac_printf: %d %-*.5s %i %%\n", 13, 18, "testing 123", -4078);
+    ft_printf("ft_printf: %d %-*.13ls %i %5.5%\n", 13, 18, L"ζέες καὶ μυρτιὲς δὲν θὰ βρῶ πιὰ στὸ χ", -4078);
+       printf("ac_printf: %d %-*.13ls %i %5.5%\n", 13, 18, L"ζέες καὶ μυρτιὲς δὲν θὰ βρῶ πιὰ στὸ χ", -4078);
+    ft_printf("ft_printf: %d %-*.13s %i %5.5%\n", 13, 18, "", -4078);
+       printf("ac_printf: %d %-*.13s %i %5.5%\n", 13, 18, "", -4078);
+    ft_printf("ft_printf: %d %*.13s %i %5.5%\n", 13, 18, NULL, -4078);
+       printf("ac_printf: %d %*.13s %i %5.5%\n", 13, 18, NULL, -4078);
+    ft_printf("ft_printf: %d %-*.13ls %i %5.5%\n", 13, 18, L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B", -4078);
+       printf("ac_printf: %d %-*.13ls %i %5.5%\n", 13, 18, L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B", -4078);
+    ft_printf("ft_printf: %S\n", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");
+       printf("ac_printf: %S\n", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");*/
+    
 	double d = .19487534534367567;
+	printf("pp%.15a\n", .75);
+	ft_printf("ft%.15a\n", .75);
+	printf("pp%.a\n", d);
+	ft_printf("ft%.a\n", d);
+	printf("pp%.15a\n", d);
+	ft_printf("ft%.15a\n", d);
+	printf("pp%.5a\n", d);
+	ft_printf("ft%.5a\n", d);
+	printf("pp{%A}\n", 1.42);
+	ft_printf("ft{%A}\n", 1.42);
+	printf("pp{%.6a}\n", 1.42);
+	ft_printf("ft{%.6a}\n", 1.42);
+	printf("pp{%.6a}\n", 0.0);
+	ft_printf("ft{%.6a}\n", 0.0);
+	printf("pp%.15g\n", d);
+	ft_printf("ft%.15g\n", d);
+	printf("pp%.5g\n", d);
+	ft_printf("ft%.5g\n", d);
+	printf("pp{%g}\n", 1.42);
+	ft_printf("ft{%g}\n", 1.42);
+	printf("pp{%.6G}\n", 1.42);
+	ft_printf("ft{%.6G}\n", 1.42);
+	printf("pp{%.6G}\n", 0.0);
+	ft_printf("ft{%.6G}\n", 0.0);
 	printf("pp%.15f\n", d);
 	ft_printf("ft%.15f\n", d);
 	printf("pp%.5f\n", d);
